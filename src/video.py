6c4b07231605453c5072debbda2ed98413a2edc8
Sasha <poriadchenko@yandex.ru>
2024-02-28 23:31:17 +0300

    1

--- src/video.py
+++ src/video.py
@@ -1,1 +1,32 @@
-1
+import os
+
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key = os.getenv('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
